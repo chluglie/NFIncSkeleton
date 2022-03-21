2f8fca3c26ce62fded012b91677ce05d99c6fa2a
Femi Oliyide <P2626686@my365.dmu.ac.uk>
2022-03-21 23:59:45 +0000

    added testing for Customer class

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public bool CustomerStatus { get; set; }
+        public DateTime CustomerDOB { get; set; }
+        public string CustomerAddress { get; set; }
+        public int CustomerMobileNumber { get; set; }
+    }
+}
+
