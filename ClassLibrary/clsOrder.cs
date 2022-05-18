251cc62047ded073cfd05de32d0bb539b1ba5779
Charlie Richardson <P2604619@my365.dmu.ac.uk>
2022-05-18 13:24:24 +0100

    Editted clsOrder and tstOrder - now tests its own hard coded data sets.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -4,13 +4,36 @@ namespace ClassLibrary
 {
     public class clsOrder
     {
-        public bool Active { get; set; }
-        public DateTime Date { get; set; }
-        public int Quantity { get; set; }
-        public double Price { get; set; }
-        public string Name { get; set; }
-        public object CustomerId { get; set; }
-        
+        private bool mActive;
+        public bool Active
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mActive;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mActive = value;
+            }
+        }
+
+        private DateTime mDateAdded;
+        public DateTime DateAdded
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDateAdded;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDateAdded = value;
+            }
+        }
+
         //private data member for the orderid property
         private Int32 mOrderId;
         //OrderId public property
@@ -28,10 +51,77 @@ namespace ClassLibrary
             }
         }
 
-        public bool Find(object OrderId)
+        private Int32 mCustomerId;
+        public int CustomerId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mCustomerId;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mCustomerId = value;
+            }
+        }
+
+        private string mItemName;
+        public string ItemName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mItemName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mItemName = value;
+            }
+        }
+
+        private Int32 mQuantity;
+        public Int32 Quantity
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mQuantity;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mQuantity = value;
+            }
+        }
+
+        private Double mPrice;
+        public Double Price
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mPrice;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mPrice = value;
+            }
+        }
+
+
+        public bool Find(int OrderId)
         {
             //set the private data members to the test data value
             mOrderId = 5;
+            mCustomerId = 20;
+            mItemName = "Clipper";
+            mDateAdded = Convert.ToDateTime("16/4/2022");
+            mQuantity = 12;
+            mPrice = 3.95;
+            mActive = true;
             //will cause the program to always run true
             return true;
         }
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -158,7 +158,107 @@ namespace TestingOrder
             Assert.IsTrue(OK);
 
         }
+        [TestMethod]
+        public void TestCustomerNoFound() //This test finds the a record
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 OrderId = 5;
+            //invoke the method
+            Found = AnOrder.Find(OrderId);
+            //check the quantity no
+            if (AnOrder.CustomerId != 20)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
 
+        }
 
+        [TestMethod]
+        public void TestItemNameFound() //This test finds the a record
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 OrderId = 5;
+            Found = AnOrder.Find(OrderId);
+            if (AnOrder.ItemName != "Clipper")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestDateFound() //This test finds the a record
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 OrderId = 5;
+            Found = AnOrder.Find(OrderId);
+            if (AnOrder.DateAdded != Convert.ToDateTime("16/04/2022"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestQuantityNoFound() //This test finds the a record
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 OrderId = 5;
+            Found = AnOrder.Find(OrderId);
+            if (AnOrder.Quantity != 12)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestPriceNoFound() //This test finds the a record
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 OrderId = 5;
+            Found = AnOrder.Find(OrderId);
+            if (AnOrder.Price != 3.95)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestOrderConfirmedFound() //This test finds the a record
+        {
+            clsOrder AnOrder = new clsOrder();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 OrderId = 5;
+            Found = AnOrder.Find(OrderId);
+            if (AnOrder.Active != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
     }
 }
