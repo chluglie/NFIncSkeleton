d0fe9b9298f0a7eb7838f39d560efc85cab52892
Charlie Richardson <P2604619@my365.dmu.ac.uk>
2022-05-19 15:41:51 +0100

    Added tests for clsOrderCollection

--- Testing2/clsOrderCollection.cs
+++ Testing2/clsOrderCollection.cs
@@ -1,6 +1,109 @@
-﻿namespace TestingOrderCollection
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace TestingOrderCollection
 {
-    internal class clsOrderCollection
+    public class clsOrderCollection
     {
+        //creating the cosntructor
+        public clsOrderCollection()
+        {
+            //This is the first version, however the updated version using the data base is commented out beneath.
+            //This is due to not having access to the time of the database at the time of coding. However, the code as been built so it
+            //can easily be swapped out and used when connected to the database.
+
+            //creating the items of test data
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.ItemName = "Clipper";
+            TestItem.OrderId = 1;
+            TestItem.CustomerId = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Quantity = 12;
+            TestItem.Price = 3.95;
+            TestItem.Active = true;
+            //adds the item to the test list
+            mOrderList.Add(TestItem);
+            //reintialise the object for some new data
+            TestItem = new clsOrder();
+            TestItem.ItemName = "ZObbo";
+            TestItem.OrderId = 2;
+            TestItem.CustomerId = 2;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Quantity = 12;
+            TestItem.Price = 3.95;
+            TestItem.Active = true;
+            //add the item to the test list
+            mOrderList.Add(TestItem);
+
+            /*
+            public clsOrderCollection() 
+            {
+                Int32 Index = 0;
+                Int32 RecordCount = 0;
+                clsDataConnection DB = new clsDatConnection();
+                DB.execute("sproc_tblOrder_SelectAll");
+                RecordCount = DB.Count;
+                while (Index < RecordCount)
+                {
+                    clsOrder AnOrder = new clsOrder();
+                    AnOrder.OrderId = Convert.ToInt32(DB.DataTable.Rows[0]["OrderId"]); ;
+                    AnOrder.mCustomerId = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerId"]);
+                    AnOrder.mItemName = Convert.ToString(DB.DataTable.Rows[0]["ItemName"]);
+                    AnOrder.mDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["DateAdded"]);
+                    AnOrder.mQuantity = Convert.ToInt32(DB.DataTable.Rows[0]["Quantity"]);
+                    AnOrder.mPrice = Convert.ToDouble(DB.DataTable.Rows[0]["Price"]);
+                    AnOrder.mActive = Convert.ToBoolean(DB.DataTable.Rows[0]["Active"]);
+                    mOrderList.Add(AnOrder);
+                    Index++;
+                }
+            }
+             */
+        }
+
+
+
+
+
+        //private data member for the list
+        List<clsOrder> mOrderList = new List<clsOrder>();
+
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+                //do later
+            }
+        }
+
+        private clsOrder mThisOrder;
+        public clsOrder ThisOrder 
+        { 
+            get 
+            {
+                return mThisOrder;
+            } 
+            set 
+            {
+                mThisOrder = value;
+            } 
+        }
     }
 }
--- Testing2/tstOrderCollection.cs
+++ Testing2/tstOrderCollection.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassLibrary;
-using System;
+using System.Collections.Generic;
 
 namespace TestingOrderCollection
 {
@@ -11,6 +12,85 @@ namespace TestingOrderCollection
         public void InstanceOK()
         {
             clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.IsNotNull(AllOrders);
+        }
+
+        [TestMethod]
+        public void OrderListOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add items to the list
+            clsOrder TestItem = new clsOrder();
+            //set the data for the test property
+            TestItem.ItemName = "Clipper";
+            TestItem.OrderId = 1;
+            TestItem.CustomerId = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Quantity = 12;
+            TestItem.Price = 3.95;
+            TestItem.Active = true;
+            //add the test item to the list
+            TestList.Add(TestItem);
+            //assign data to property and test the values
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+        
+        
+        [TestMethod]
+        public void CountOrderOK()  //This test is made redundant by the updated version of clsOrderCollection, inside clsOrderCollection.cs
+        {                           //When linked to the database this test should be removed as it will fail.
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Int32 TestCount = 2; //quick fix just to make the test pass
+            AllOrders.Count = TestCount;
+            Assert.AreEqual(AllOrders.Count, TestCount);
+        }
+
+        [TestMethod]
+        public void ThisOrderOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //add items to the list
+            clsOrder TestOrder = new clsOrder();
+            //set the data for the test property
+            TestOrder.ItemName = "Clipper";
+            TestOrder.OrderId = 1;
+            TestOrder.CustomerId = 1;
+            TestOrder.DateAdded = DateTime.Now.Date;
+            TestOrder.Quantity = 12;
+            TestOrder.Price = 3.95;
+            TestOrder.Active = true;
+            //add the data to the list
+            AllOrders.ThisOrder = TestOrder;
+            Assert.AreEqual(AllOrders.ThisOrder, TestOrder);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            //adding some test data
+            TestItem.ItemName = "Clipper";
+            TestItem.OrderId = 1;
+            TestItem.CustomerId = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Quantity = 12;
+            TestItem.Price = 3.95;
+            TestItem.Active = true;
+            //adding the item to the test list
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent() //This test is made redundant by the updated version of clsOrderCollection, inside clsOrderCollection.cs
+        {                               //When linked to the database this test should be removed as it will fail.
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.AreEqual(AllOrders.Count, 2);
         }
     }
 }
